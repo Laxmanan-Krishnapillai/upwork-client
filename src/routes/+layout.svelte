33dc89a4bb52035c68198b7d48de9d026cc59564
Sivalaxmanan Krishnapillai <65386461+Laxmanan-Krishnapillai@users.noreply.github.com>
2023-08-03 17:34:18 +0000

    Update 11 files

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+<svelte:head>
+    <script async src="/script/main.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/Draggable.min.js"></script>
+    <script src="/script/jquery.min.js"></script>
+    <script src="/script/gsap/scrollsmoother.min.js"></script>
+    <script src="/script/gsap/inertia.min.js"></script>
+    <script src="/script/load.js"></script>
+</svelte:head>
+<slot />
